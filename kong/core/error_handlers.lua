a9c10907480d2ab2f17bc1cfd4c3c1710df3adcd
dheeraj-ai <dheeraj@infrrd.ai>
2017-07-21 15:27:41 +0530

    custom message

--- kong/core/error_handlers.lua
+++ kong/core/error_handlers.lua
@@ -14,21 +14,6 @@ local json_template = '{"message":"%s"}'
 local xml_template = '<?xml version="1.0" encoding="UTF-8"?>\n<error><message>%s</message></error>'
 local html_template = '<html><head><title>Kong Error</title></head><body><h1>Kong Error</h1><p>%s.</p></body></html>'
 
-local BODIES = {
-  s404 = "Not found",
-  s408 = "Request timeout",
-  s411 = "Length required",
-  s412 = "Precondition failed",
-  s413 = "Payload too large",
-  s414 = "URI too long",
-  s417 = "Expectation failed",
-  s500 = "An unexpected error occurred",
-  s502 = "An invalid response was received from the upstream server",
-  s503 = "The upstream server is currently unavailable",
-  s504 = "The upstream server is timing out",
-  default = "The upstream server responded with %d"
-}
-
 local SERVER_HEADER = _KONG._NAME .. "/" .. _KONG._VERSION
 
 return function(ngx)
